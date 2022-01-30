1bf10713b0897d59f752a3ef761a5b950f76f413
pavan kumar punna <67155391+punnapavankumar9@users.noreply.github.com>
2022-01-30 23:35:58 +0530

    added custom templates for password reset system

--- base/views.py
+++ base/views.py
@@ -16,8 +16,7 @@ from base.forms import RoomForm
 # ]
 
 def home(request):
-    q= request.GET.get('q') if request.GET.get('q') != None else ""
-
+    q = request.GET.get('q') if request.GET.get('q') != None else ""
     rooms = Room.objects.filter(
                 Q(topic__name__icontains = q) | 
                 Q(name__icontains = q)|
