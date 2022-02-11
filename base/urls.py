1624dba59a7ba13a20f25da169c42cf7ee8b82c4
pavan kumar punna <67155391+punnapavankumar9@users.noreply.github.com>
2022-02-11 16:29:44 +0530

    updated api according to private room features

--- base/urls.py
+++ base/urls.py
@@ -4,15 +4,12 @@ app_name = 'base'
 
 urlpatterns = [
     path('', views.home, name="home"),
-    path('about/', views.about, name="about"),
     path('room/<str:pk>/', views.room, name="room"),
     path('create_room/', views.create_room, name="create-room"),
     path('update_room/<str:pk>/', views.update_room, name='update-room'),
     path('delete_room/<str:pk>/', views.delete_room, name='delete-room'),
     path('delete_message/<str:pk>/', views.delete_message, name='delete-message'),
-
     path('topics/', views.topics_view, name="topics"),
     path('activity/', views.activity_view, name="activity"),
     path('room_requests/<str:room_id>/', views.room_requests, name="requests"),
-
 ]
